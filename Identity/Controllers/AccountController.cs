f5be52b6ef4b00918493062651dadb22b7fd3fe9
Ehsan Seyedzadeh <e.seyedzadeh73@Gmail.com>
2022-09-15 14:34:41 +0430

    Admin User Management

--- Identity/Controllers/AccountController.cs
+++ Identity/Controllers/AccountController.cs
@@ -13,7 +13,8 @@ namespace Identity.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
-        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        public AccountController(UserManager<User> userManager, 
+            SignInManager<User> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
